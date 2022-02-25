4e130787a32764048bb690d71dbf73779c31574b
fayaz <fayazakthar70@gmail.com>
2022-02-25 20:41:08 +0530

    Designed Market Page

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-app.debug = True
-
-@app.route("/")
-@app.route("/home")
-def hello_world():
-    return render_template('home.html')
